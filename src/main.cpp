91d415fe128f2fb03dc184c0285471c4272d7301
Redrch <1951381703@qq.com>
2026-01-11 19:33:06 +0800

    添加了Version 1.2.0版本的TODO

--- src/main.cpp
+++ src/main.cpp
@@ -20,3 +20,19 @@ int main(int argc, char *argv[]) {
     logger.shutdown();
     return ret;
 }
+
+
+/*
+Version 1.2.0 TO-DO
+TODO: 自动搜索Python解释器路径
+TODO: 自动配置输出路径
+TODO: 添加程序信息配置
+TODO: 新建项目时自动安装nuitka
+TODO: 重构打包UI界面
+TODO: 重构设置UI页面（如果需要）
+TODO: 添加打包日志功能，数据存储在npf文件中
+TODO: 添加新增项的配置
+TODO: 添加最小化到系统托盘的功能
+TODO: 添加将打包/停止按钮吸附在其他窗口上的功能
+暂时先写这么多
+*/
